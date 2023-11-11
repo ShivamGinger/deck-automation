e8b91d01bb43cb63412fda397d407bb4609bd437
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-11 14:52:56 +0530

    Candidate retrieval, creation added

--- src/app/api/candidates/route.ts
+++ src/app/api/candidates/route.ts
@@ -10,30 +10,44 @@ import { candidates } from "@/db/schema";
 import { createCandidateSchema } from "@/utils/bodyValidationSchemas";
 
 export async function GET(request: NextRequest) {
-
-  const candidates = await getAllCandidates();
-
-  return NextResponse.json(candidates);
-}
+  try {
+    const candidates = await getAllCandidates();
+    if (candidates.length === 0) {
+      return NextResponse.json({ error: 'No candidates found' }, { status: 404 });
+    }
+    return NextResponse.json({ data: candidates });
+  } catch (error: any) {
+      console.error('Error fetching candidate:', error);
+      return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    };
+};
 
 export async function POST(request: NextRequest) {
   try {
     const requestData = await request.json();
-
     const parsedData = createCandidateSchema.parse(requestData);
-
-    const addCandidate = await db.insert(candidates).values(parsedData);
-
-    if (addCandidate) {
-      return NextResponse.json({ message: "Data added successfully", data: parsedData });
-    } else {
-      return NextResponse.json({ error: "Data insertion failed" }, { status: 500 });
+    const candidate = await db.insert(candidates).values({
+      name: parsedData.name,
+      email: parsedData.email,
+      keyPoints: parsedData.keyPoints,
+      profilePic: parsedData.profilePic,
+      social: parsedData.social,
+      currPos: parsedData.currPos,
+      currLoc: parsedData.currLoc,
+      experience: parsedData.experience,
+      phNum: parsedData.phNum,
+      fixedLpa: parsedData.fixedLpa,
+      varLpa: parsedData.varLpa,
+      expectedCtc: parsedData.expectedCtc,
+      noticePeriod: parsedData.noticePeriod
     }
+    );
 
+    return NextResponse.json({ message: "Data added successfully", data: parsedData }, { status: 201 });
   } catch (error: any) {
-
     if (error instanceof ZodError) {
-
+      console.log(error.issues);
+      
       const errorDetails = error.issues.map((issue) => ({
         field: issue.path.join("."),
         message: issue.message,
@@ -45,4 +59,4 @@ export async function POST(request: NextRequest) {
       return NextResponse.json({ error: "Invalid request", details: error.message }, { status: 400 });
     }
   }
-}
+};
