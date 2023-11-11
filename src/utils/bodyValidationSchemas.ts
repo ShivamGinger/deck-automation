7491fd6b6ec8825e5a497b44ecc53756e9081e0c
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-11 15:00:17 +0530

    validation for company,candidate,role

--- src/utils/bodyValidationSchemas.ts
+++ src/utils/bodyValidationSchemas.ts
@@ -3,24 +3,46 @@ import { z } from "zod";
 // Define all the schemas for body parsing here
 
 export const createCandidateSchema = z.object({
+  id: z.number().positive(),
   name: z.string(),
-  keyPoints: z.array(z.string()),
-  profilePic: z.string(),
-  social: z.string(),
-  companyId: z.number(),
-  roleId: z.number()
+  phNum: z.string().min(10).max(14),
+  email: z.string().email(),
+  keyPoints: z.record(z.string()).nullable(),
+  profilePic: z.string().nullable(),
+  social: z.string().nullable(),
+  companyId: z.number().nullable(),
+  roleId: z.number().nullable(),
+  createdAt: z.string().nullable(),
+  currPos: z.string().nullable(),
+  currLoc: z.string().nullable(),
+  experience: z.string().nullable(),
+  fixedLpa: z.string().nullable(),
+  varLpa: z.string().nullable(),
+  expectedCtc: z.string().nullable(),
+  noticePeriod: z.string().nullable()
 });
 
-export const userRegistrationSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(6)
+export const deleteCandidateSchema = z.object({
+  id: z.number().positive()
 });
 
 export const createCompanySchema = z.object({
+  id: z.number().positive(),
   name: z.string(),
+  createdAt: z.string().nullable()
+});
+
+export const deleteCompanySchema = z.object({
+  id: z.number().positive()
 });
 
 export const createRoleSchema = z.object({
-  companyID: z.string().regex(/^[0-9]+$/),
-  name: z.string().regex(/^[A-Za-z]+$/),
+  id: z.number().positive(),
+  name: z.string(),
+  companyId: z.number(),
+  createdAt: z.string().nullable()
+});
+
+export const deleteRoleSchema = z.object({
+  id: z.number().positive()
 });
