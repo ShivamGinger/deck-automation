798c413c1fe24209028ddd03399f54dbe86bc80c
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-13 13:31:11 +0530

    company creation, retrieval

--- src/lib/companies.ts
+++ src/lib/companies.ts
@@ -1,8 +1,21 @@
 import { db } from '@/db';
-import { Companies } from '@/db/schema';
+import { Companies, companies } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 export default async function getAllCompanies(): Promise<Companies[]> {
-    const result: Companies[] = await db.query.companies.findMany();
-  
-    return result;
+    const companiesAll: Companies[] = await db.query.companies.findMany();
+
+    return companiesAll;
 }
+
+export async function getCompany(id: number): Promise<Companies[]> {
+    const company: Companies[] = await db.select().from(companies).where(eq(companies.id, id));
+    
+    return company;
+};
+
+export async function getCompanyByName(name: string): Promise<Companies[]> {
+    const company: Companies[] = await db.select().from(companies).where(eq(companies.name, name));
+    
+    return company;
+};
