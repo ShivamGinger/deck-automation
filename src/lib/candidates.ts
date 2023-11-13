1d8faecbcd1cafe701eb11f7b6b054af0727028c
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-13 13:30:44 +0530

    candidate retrieval, creation

--- src/lib/candidates.ts
+++ src/lib/candidates.ts
@@ -1,10 +1,69 @@
 import { db } from '@/db';
 
-import { Candidates } from '@/db/schema';
+import { Candidates, candidates, companies, roles } from '@/db/schema';
+import { eq } from 'drizzle-orm';
 
 export default async function getAllCandidates(): Promise<Candidates[]> {
 
-  const result: Candidates[] = await db.query.candidates.findMany();
+  const result: Candidates[] = await db.select({
+    id: candidates.id,
+    name: candidates.name,
+    email: candidates.email,
+    phNum: candidates.phNum,
+    companyId: candidates.companyId,
+    company: companies.name,
+    roleId: candidates.roleId,
+    role: roles.name,
+    keyPoints: candidates.keyPoints,
+    profilePic: candidates.profilePic,
+    social: candidates.social,
+    experience: candidates.experience,
+    currPos:candidates.currPos,
+    currLoc: candidates.currLoc,
+    fixedLpa: candidates.fixedLpa,
+    varLpa: candidates.varLpa,
+    expectedCtc: candidates.expectedCtc,
+    noticePeriod: candidates.noticePeriod,
+    description: candidates.description,
+    achievement: candidates.achievement,
+    createdAt: candidates.createdAt
+  })
+  .from(candidates)
+  .leftJoin(companies, eq(candidates.companyId, companies.id))
+  .leftJoin(roles, eq(candidates.roleId, roles.id))
 
   return result;
-}
+};
+
+export async function getCandidate(id: number): Promise<Candidates[]> {
+  
+  const result: Candidates[] = await db.select({
+    id: candidates.id,
+    name: candidates.name,
+    email: candidates.email,
+    phNum: candidates.phNum,
+    companyId: candidates.companyId,
+    company: companies.name,
+    roleId: candidates.roleId,
+    role: roles.name,
+    keyPoints: candidates.keyPoints,
+    profilePic: candidates.profilePic,
+    social: candidates.social,
+    experience: candidates.experience,
+    currPos:candidates.currPos,
+    currLoc: candidates.currLoc,
+    fixedLpa: candidates.fixedLpa,
+    varLpa: candidates.varLpa,
+    expectedCtc: candidates.expectedCtc,
+    noticePeriod: candidates.noticePeriod,
+    description: candidates.description,
+    achievement: candidates.achievement,
+    createdAt: candidates.createdAt
+  })
+  .from(candidates)
+  .leftJoin(companies, eq(candidates.companyId, companies.id))
+  .leftJoin(roles, eq(candidates.roleId, roles.id))
+  .where(eq(candidates.id, id));
+
+  return result;
+};
