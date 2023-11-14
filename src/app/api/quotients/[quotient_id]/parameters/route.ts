d6cd51e37d1193babcc70c5d261ac85de3cfdffd
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-14 20:30:31 +0530

    param GET fix

--- src/app/api/quotients/[quotient_id]/parameters/route.ts
+++ src/app/api/quotients/[quotient_id]/parameters/route.ts
@@ -6,15 +6,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 //TODO: add a function, route to display the weightages of different parameters and quotients based on company and their roles.
 
-export async function GET(request: NextRequest, { param }: { param: { quotient_id: number }}) {
+export async function GET(request: NextRequest, { params }: { params: { quotient_id: number }}) {
     try{
-        const qSlug = param.quotient_id;
-        const params: qParams[] = await getAllQuotientParams(qSlug);
-        if (params.length == 0){
+        const qSlug = params.quotient_id;
+        const param: qParams[] = await getAllQuotientParams(qSlug);
+        if (param.length === 0){
             return NextResponse.json({ error: `Parameters not found.` }, { status: 404 });
         }
+        // return NextResponse.json({ data: "params" }, { status: 200});
     
-        return NextResponse.json({ data: params }, { status: 200});
+        return NextResponse.json({ data: param }, { status: 200});
     } catch (error: any) {
           console.error('Error fetching parameters:', error);
           return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
