f158bdc2c77efe3fb1cf1a30880bfb3fdcee072c
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-15 16:03:41 +0530

    candidate creation, return fixed

--- src/app/api/candidates/route.ts
+++ src/app/api/candidates/route.ts
@@ -5,9 +5,10 @@ import { NextRequest, NextResponse } from "next/server";
 import getAllCandidates from "@/lib/candidates";
 
 import { db } from "@/db";
-import { candidates } from "@/db/schema";
+import { candidates, roles } from "@/db/schema";
 
 import { createCandidateSchema } from "@/utils/bodyValidationSchemas";
+import { sql, eq } from "drizzle-orm";
 
 export async function GET(request: NextRequest) {
   try {
@@ -25,40 +26,42 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   try {
     const requestData = await request.json();
-    const parsedData = createCandidateSchema.parse(requestData);
-    const candidate = await db.insert(candidates).values({
-      name: parsedData.name,
-      email: parsedData.email,
-      roleId: parsedData.roleId,
-      companyId: parsedData.companyId,
-      keyPoints: parsedData.keyPoints,
-      profilePic: parsedData.profilePic,
-      social: parsedData.social,
-      currPos: parsedData.currPos,
-      currLoc: parsedData.currLoc,
-      experience: parsedData.experience,
-      phNum: parsedData.phNum,
-      fixedLpa: parsedData.fixedLpa,
-      varLpa: parsedData.varLpa,
-      expectedCtc: parsedData.expectedCtc,
-      noticePeriod: parsedData.noticePeriod
-    }
-    );
+    const parsedData = createCandidateSchema.safeParse(requestData);
+    if (!parsedData.success) {
+      return NextResponse.json({ message: "Validation Error", error: `${parsedData.error}` }, { status: 400 });
+    };
+    for( const data of parsedData.data.candidateInfo) {
+      await db.insert(candidates).values({
+        name: data.name,
+        keyPoints: {"keyPoints": data.keyPoints},
+        profilePic: data.profilePic,
+        social: data.social,
+        companyId: data.companyId,
+        roleId: data.roleId,
+        email: data.email,
+        currPos: data.currPos,
+        currLoc: data.currLoc,
+        experience: data.experience,
+        phNum: data.phNum,
+        fixedLpa: data.fixedLpa,
+        varLpa: data.varLpa,
+        expectedCtc: data.expectedCtc,
+        noticePeriod: data.noticePeriod,
+        description: data.description,
+        achievement: {"achievement": data.achievement},
+        gender: data.gender,
+        currCmp: data.currCmp,
+        esopRsu: data.esopRsu
+      }
+      );
+    };
 
-    return NextResponse.json({ message: "Data added successfully", data: parsedData }, { status: 201 });
+    const candId: any = await db.execute(sql`SELECT LAST_INSERT_ID()`);
+    const crtCand = await db.select().from(candidates).where(eq(candidates.id, candId.rows[0]["LAST_INSERT_ID()"]))
+    
+    return NextResponse.json({ message: "Data added successfully", data: crtCand }, { status: 201 });
+  
   } catch (error: any) {
-    if (error instanceof ZodError) {
-      console.log(error.issues);
-      
-      const errorDetails = error.issues.map((issue) => ({
-        field: issue.path.join("."),
-        message: issue.message,
-      }));
-
-      return NextResponse.json({ error: "Validation error", details: errorDetails }, { status: 400 });
-    }
-    else {
       return NextResponse.json({ error: "Invalid request", details: error.message }, { status: 400 });
-    }
   }
 };
