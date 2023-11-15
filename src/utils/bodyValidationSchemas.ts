8b0dd919180ee22927d2d2b0b5bd92950cd157ad
ShivamTaneja <shiivamtaneja@gmail.com>
2023-11-15 15:07:05 +0530

    update create candiate schema

--- src/utils/bodyValidationSchemas.ts
+++ src/utils/bodyValidationSchemas.ts
@@ -3,26 +3,30 @@ import { z } from "zod";
 // Define all the schemas for body parsing here
 
 export const createCandidateSchema = z.object({
-  name: z.string(),
-  keyPoints: z.array(z.string()).nullable(),
-  profilePic: z.string().nullable(),
-  companyId: z.number().nullable(),
-  roleId: z.number().nullable(),
-  social: z.string().nullable(),
-  email: z.string().email(),
-  currPos: z.string().nullable(),
-  currLoc: z.string().nullable(),
-  experience: z.string().nullable(),
-  phNum: z.string().min(10).max(14),
-  fixedLpa: z.number().multipleOf(0.01).nullable(),
-  varLpa: z.number().multipleOf(0.01).nullable(),
-  expectedCtc: z.string().nullable(),
-  noticePeriod: z.string().nullable(),
-  description: z.string().nullable(),
-  achievement: z.array(z.string()).nullable(),
-  gender: z.enum(['male', 'female', 'other']),
-  currCmp: z.string().nullable(),
-  esopRsu: z.number().multipleOf(0.01).nullable(),
+  candidateInfo: z.array(
+    z.object({
+      name: z.string(),
+      keyPoints: z.array(z.string()).nullable(),
+      profilePic: z.string().nullable(),
+      companyId: z.number().nullable(),
+      roleId: z.number().nullable(),
+      social: z.string().nullable(),
+      email: z.string().email(),
+      currPos: z.string().nullable(),
+      currLoc: z.string().nullable(),
+      experience: z.string().nullable(),
+      phNum: z.string().min(10).max(14),
+      fixedLpa: z.number().multipleOf(0.01).nullable(),
+      varLpa: z.number().multipleOf(0.01).nullable(),
+      expectedCtc: z.string().nullable(),
+      noticePeriod: z.string().nullable(),
+      description: z.string().nullable(),
+      achievement: z.array(z.string()).nullable(),
+      gender: z.enum(['male', 'female', 'other']),
+      currCmp: z.string().nullable(),
+      esopRsu: z.number().multipleOf(0.01).nullable(),
+    })
+  )
 });
 
 export const deleteCandidateSchema = z.object({
