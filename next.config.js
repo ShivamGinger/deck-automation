942977b1c8cab07f1a140cf815b963452b0c5037
ShivamTaneja <shiivamtaneja@gmail.com>
2023-11-16 21:58:45 +0530

    add S3 AWS profile pic upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['deck-automation-profile-pictures-storage.s3.ap-south-1.amazonaws.com']
+    // domains: ['deck-automation-profile-pictures-storage.s3.ap-south-1.amazonaws.com', 'demo-images-bucket-stream-helper.s3.ap-south-1.amazonaws.com']
+    domains : ['demo-images-bucket-stream-helper.s3.ap-south-1.amazonaws.com']
   }
 }
 
--- src/app/api/s3-upload/route.ts
+++ src/app/api/s3-upload/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-import { GetObjectCommand, PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client, S3ClientConfig } from "@aws-sdk/client-s3";
 import { randomBytes } from "crypto";
 
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
