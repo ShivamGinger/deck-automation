9441477a360f7224b2a3d453c3e94bc98404c0bb
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-17 13:26:12 +0530

    refactoring

--- src/lib/parameters.ts
+++ src/lib/parameters.ts
@@ -1,56 +1,48 @@
 import { db } from "@/db";
-import { companies, parameters, Parameters, parameterWeightages, roles } from "@/db/schema";
+import { companies, parameters, Parameters, parameterWeightages, quotients, roles } from "@/db/schema";
 import { eq, and } from "drizzle-orm";
 
 export type qParams = {
-    id: number;
-    parameter: string;
     qid: number;
+    qname: string;
+    pid: number;
+    pname: string;
 };
 
-export type paramWDispSchema = {
+export type paramExistType = {
     pid: number;
-    pwid: number;
-    pname: string;
-    cname: string;
-    rname: string | null;
-    weightage: number;
+    qid: number;
 };
 
 export async function getAllQuotientParams(qSlug: number): Promise<qParams[]> {
     const quoParam: qParams[] = await db.select({
-        id: parameters.id,
-        parameter: parameters.parameter,
-        qid: parameters.quotientId
+        qid: parameters.quotientId,
+        qname: quotients.quotient,
+        pid: parameters.id,
+        pname: parameters.parameter
     })
     .from(parameters)
+    .innerJoin(quotients, eq(quotients.id, parameters.quotientId))
     .where(eq(parameters.quotientId, qSlug));
 
     return quoParam;
 };
 
-export async function getParameters(qid: number, param: string): Promise<Parameters[]> {
+export async function getParameters(qid: number, pid: string): Promise<Parameters[]> {
     const paramExist: Parameters[] = await db.select()
     .from(parameters)
-    .where(and(eq(parameters.quotientId, qid), eq(parameters.parameter, param)));
+    .where(and(eq(parameters.quotientId, qid), eq(parameters.parameter, pid)));
 
     return paramExist;
 };
 
-export async function getParameterById(qid: number, pid: number): Promise<paramWDispSchema[]> {
-    const parameter: paramWDispSchema[] = await db.select({
+export async function getParameterById(qid: number, pid: number): Promise<paramExistType[]> {
+    const paramExist: paramExistType[] = await db.select({
         pid: parameters.id,
-        pwid: parameterWeightages.id,
-        pname: parameters.parameter,
-        cname: companies.name,
-        rname: roles.name,
-        weightage: parameterWeightages.pWeightage
+        qid: quotients.id
     })
-    .from(parameterWeightages)
-    .innerJoin(parameters, eq(parameters.id, parameterWeightages.parameterId))
-    .innerJoin(companies, eq(companies.id, parameterWeightages.companyId))
-    .innerJoin(roles, eq(roles.id, parameterWeightages.roleId))
+    .from(parameters)
     .where(and(eq(parameters.id, pid), eq(parameters.quotientId, qid)));
 
-    return parameter;
+    return paramExist;
 };
