37431b51de599a37a273a26a443596b21b415c5b
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-21 15:38:18 +0530

    quotient GET, DELETE endpoint added

--- src/app/api/companies/[company_id]/roles/[role_id]/quotients/[quotient_id]/route.ts
+++ src/app/api/companies/[company_id]/roles/[role_id]/quotients/[quotient_id]/route.ts
@@ -1,1 +1,31 @@
-//TODO: Implement GET, DELETE, possibly PUT endpoint here
+import { db } from "@/db";
+import { quotientWeightages } from "@/db/schema";
+import { getCmpQuotient, quotientw } from "@/lib/quotients";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+//TODO: Implement PUT endpoint here
+
+export async function GET(request: NextRequest, { params }: {params: { company_id: number, role_id: number, quotient_id: number}}) {
+    try{
+        const quotient: quotientw[] = await getCmpQuotient(params.company_id, params.role_id, params.quotient_id);
+        return NextResponse.json({ data: quotient }, { status: 200});
+    } catch (error: any) {
+          console.error('Error fetching quotients:', error);
+          return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+      };
+};
+
+export async function DELETE(request: NextRequest, { params }: {params: { company_id: number, role_id: number, quotient_id: number}}) {
+    try{
+        const quotient: quotientw[] = await getCmpQuotient(params.company_id, params.role_id, params.quotient_id);
+        if(quotient.length === 0) {
+            return NextResponse.json({ error: 'Weightage for the quotient does not exist' }, { status: 404 });
+        };
+        await db.delete(quotientWeightages).where(eq(quotientWeightages.id, quotient[0].quotient_weightage_id));
+        return NextResponse.json({ status: 204});
+    } catch (error: any) {
+          console.error('Error deleting quotient weightage:', error);
+          return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+      };
+};
