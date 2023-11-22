31e4e8356a96168a8840846a983fecf850154c49
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-22 15:47:00 +0530

    changes to schema names for candidate creation

--- src/utils/bodyValidationSchemas.ts
+++ src/utils/bodyValidationSchemas.ts
@@ -2,14 +2,12 @@ import { z } from "zod";
 
 // Define all the schemas for body parsing here
 
-export const createCandidateSchema = z.object({
-  candidateInfo: z.array(
+export const createOrphanCandidateSchema = z.object({
+  candidate_information: z.array(
     z.object({
       candidate_name: z.string(),
       key_points: z.array(z.string()).nullable(),
       profile_pic: z.string().nullable(),
-      company_id: z.number().nullable(),
-      role_id: z.number().nullable(),
       social: z.string().nullable(),
       email: z.string().email(),
       current_position: z.string().nullable(),
@@ -32,6 +30,46 @@ export const createCandidateSchema = z.object({
         candidate_round_completed: z.number().nullable(),
         candidate_reject_reason: z.string().nullable(),
       }).nullable()
+    })
+  )
+});
+
+export const createCandidateSchema = z.object({
+  candidate_information: z.array(
+    z.object({
+      candidate_name: z.string(),
+      key_points: z.array(z.string()).nullable(),
+      profile_pic: z.string().nullable(),
+      // company_id: z.number().nullable(),
+      // role_id: z.number().nullable(),
+      social: z.string().nullable(),
+      email: z.string().email(),
+      current_position: z.string().nullable(),
+      current_location: z.string().nullable(),
+      experience: z.string().nullable(),
+      phone_number: z.string().min(10).max(14),
+      fixed_lpa: z.number().multipleOf(0.01).nullable(),
+      variable_lpa: z.number().multipleOf(0.01).nullable(),
+      expected_ctc: z.string().nullable(),
+      notice_period: z.string().nullable(),
+      description: z.string().nullable(),
+      achievement: z.array(z.string()).nullable(),
+      gender: z.enum(['male', 'female', 'other']),
+      current_company: z.string().nullable(),
+      esop_rsu: z.number().multipleOf(0.01).nullable(),
+      share_candidate_status: z.boolean(),
+      candidate_status: z.object({
+        candidate_profile_share_date: z.string().nullable(),
+        candidate_status: z.enum(['yet_to_share', 'joined', 'negotiation', 'on_hold', 'feedback_pending', 'dropped_out', 'rejected', 'in_process']).nullable(),
+        candidate_round_completed: z.number().nullable(),
+        candidate_reject_reason: z.string().nullable(),
+      }).nullable(),
+      candidate_parameter_scores: z.array(
+        z.object({ 
+          parameter_id: z.number(),
+          parameter_score: z.number(),
+        })
+      )
     })
   )
 });
