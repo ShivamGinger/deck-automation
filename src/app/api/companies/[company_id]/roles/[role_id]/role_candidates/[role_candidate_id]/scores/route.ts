233729f09771b29cd2c813510e1bbe5c94ef5b83
Jasmeet <jasmeet.s@gingerpartners.co>
2023-11-23 17:52:13 +0530

    score retrieval added

---
+++ src/app/api/companies/[company_id]/roles/[role_id]/role_candidates/[role_candidate_id]/scores/route.ts
@@ -0,0 +1,43 @@
+//TODO: score retrieval for the candidate GET,PUT, DELETE(baad mai dekh liyo iska)
+
+import { Companies } from "@/db/schema";
+import { candidateParamScoreExist, candidateParamScoreList, candidateQuoScoreExist, paramScoreList, scoreExist } from "@/lib/candidates";
+import { getCompany } from "@/lib/companies";
+import { companyRoles, getCompanyRoleCandidate, getRole, roleCandidates } from "@/lib/roles";
+import { NextRequest, NextResponse } from "next/server";
+
+//candidateQuoScoreList, quoScoreList
+
+export async function GET(request: NextRequest, { params }: { params: { company_id: number, role_id: number, role_candidate_id: number}}) {
+    try{
+        const companyIdExist: Companies[] = await getCompany(params.company_id);
+        if (companyIdExist.length === 0) {
+          return NextResponse.json({ error: "Company not found" }, { status: 404 });
+        };
+        const roleIdExist: companyRoles[] = await getRole(params.company_id, params.role_id);
+        if (roleIdExist.length === 0) {
+            return NextResponse.json({ error: "Role not found" }, { status: 404 });
+          };
+        const rolesCandidate: roleCandidates[] = await getCompanyRoleCandidate(params.company_id, params.role_id, params.role_candidate_id);
+        if (rolesCandidate.length === 0) {
+            return NextResponse.json({ error: "Candidate does not exist" }, { status: 404});
+        };
+
+        const paramScoreExists: scoreExist[] = await candidateParamScoreExist(params.role_candidate_id);
+        const quoScoreExists: scoreExist[] = await candidateQuoScoreExist(params.role_candidate_id);
+        if (paramScoreExists.length === 0 && quoScoreExists.length === 0) {
+            return NextResponse.json({ error: "Scores do not exist" }, { status: 404});
+        };
+
+        const candidateParamScore: paramScoreList[] = await candidateParamScoreList(params.role_candidate_id);
+        // const candidateQuoScore: quoScoreList[] = await candidateQuoScoreList(params.role_candidate_id);
+
+        return NextResponse.json({ "candidate_id": params.role_candidate_id, 
+        "candidate_scores": candidateParamScore 
+        }, { status: 200});
+    
+    } catch (error: any) {
+        console.error('Error fetching candidate score:', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    };
+};
