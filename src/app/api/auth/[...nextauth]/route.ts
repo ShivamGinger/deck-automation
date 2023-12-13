5ed5db4958b54bf553de7c978906ecb1c45b1734
ShivamTaneja <shiivamtaneja@gmail.com>
2023-12-14 02:06:55 +0530

    add user details in JWT token next-auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,16 @@
 import { db } from "@/db";
-import { User as DBUser, users } from "@/db/schema";
+import { User as DBUser, groups, userGroups, users } from "@/db/schema";
 import bcrypt from 'bcrypt';
 import { eq } from "drizzle-orm";
-import NextAuth, { User as NextUser } from "next-auth";
+import NextAuth, { AuthOptions, User as NextUser } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 type AuthUser = NextUser & DBUser;
 
-const authOptions: any = {
+const authOptions: AuthOptions = {
+  session: {
+    strategy: 'jwt'
+  },
   providers: [
     CredentialsProvider({
       id: "credentials",
@@ -40,7 +43,53 @@ const authOptions: any = {
         }
       },
     })
-  ]
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const userGroup = await db.select({
+          can_read: groups.canRead,
+          can_edit: groups.canEdit,
+          can_create: groups.canCreate,
+          can_delete: groups.canDelete,
+          first_name: users.firstName,
+          last_name: users.lastName,
+          email: users.email
+        }).from(userGroups)
+          .innerJoin(groups, eq(groups.id, userGroups.groupId))
+          .innerJoin(users, eq(users.id, userGroups.userId))
+          .where(eq(userGroups.userId, Number(user.id)));
+
+        const { can_read, can_create, can_edit, can_delete, first_name, last_name, email } = userGroup[0];
+
+        token.user = {
+          can_read: can_read === 1 ? true : false,
+          can_create: can_create === 1 ? true : false,
+          can_edit: can_edit === 1 ? true : false,
+          can_delete: can_delete === 1 ? true : false,
+          first_name,
+          last_name,
+          email
+        };
+      }
+
+      return Promise.resolve(token);
+    },
+    async session({ session, token }) {
+
+      session.user = token.user as {
+        can_read: boolean,
+        can_create: boolean,
+        can_edit: boolean,
+        can_delete: boolean,
+        first_name: string | null,
+        last_name: string | null
+        email: string;
+      };
+
+      return Promise.resolve(session)
+    }
+  },
 };
 
 export const handler = NextAuth(authOptions) as never;
